df96596ca8effa996b311011f701f38f93af4e7f
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-03-04 14:57:50 -0300

    Update mod10_counter.v

--- Hierarchy/Level3/mod10_counter.v
+++ Hierarchy/Level3/mod10_counter.v
@@ -1,14 +1,12 @@
 module mod10_counter (
-    input wire CLK, Reset,
-    output reg [3:0] Q
+	input wire CLK, Reset, 
+	output reg [3:0] Q
 );
-    always @ (negedge CLK or posedge Reset) begin
-        if (Reset) begin
-            Q <= 4'b0000;
-        end else if (Q == 4'b1001) begin
-            Q <= 4'b0000;
-        end else begin
-            Q <= Q + 1;
-        end
-    end
+	always @ (posedge CLK) begin
+      if(Reset || Q == 4'b0000)
+      		Q <= 4'b1001;
+		else
+			Q <= Q - 1;
+	end
 endmodule
+  
