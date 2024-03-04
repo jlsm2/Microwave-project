9ac4a033135732dda7e85e42a5cde5afd1777d51
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-03-04 15:23:23 -0300

    Update mod10_ counter.v

--- Hierarchy/Level3/Mod10 Counter/mod10_ counter.v
+++ Hierarchy/Level3/Mod10 Counter/mod10_ counter.v
@@ -3,8 +3,8 @@ module mod10_counter (
 	output reg [3:0] Q
 );
 	always @ (posedge CLK) begin
-      if(Reset || Q == 4'b0000)
-      		Q <= 4'b1001;
+		if(Reset || Q == 4'b0000)
+      			Q <= 4'b1001;
 		else
 			Q <= Q - 1;
 	end
