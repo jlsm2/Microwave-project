b89c7d4169c007c13bbbe6a665ced7487522b821
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-17 20:54:21 -0300

    Update minutes_seconds_timer_tb.v

--- Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
+++ Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
@@ -5,17 +5,23 @@ module MinutesSecondsTimer_tb;
 
     reg CLK;
     reg Reset;
+    reg Load; // New Load input
+    reg Enable; // New Enable input
 
     wire [3:0] seconds_units;
     wire [2:0] seconds_tens;
     wire [3:0] minutes_units;
+    wire timer_done; // New timer_done output
 
     MinutesSecondsTimer uut (
         .CLK(CLK),
         .Reset(Reset),
+        .Load(Load), // Connect Load input
+        .Enable(Enable), // Connect Enable input
         .seconds_units(seconds_units),
         .seconds_tens(seconds_tens),
-        .minutes_units(minutes_units)
+        .minutes_units(minutes_units),
+        .timer_done(timer_done) // Connect timer_done output
     );
 
     initial begin
@@ -31,14 +37,22 @@ module MinutesSecondsTimer_tb;
 
     initial begin
         Reset = 1;
+        Load = 1; // Load initial values
+        Enable = 0; // Don't start the timer yet
 
         #10 Reset = 0;
+        Load = 0; // Stop loading initial values
+        Enable = 1; // Start the timer
 
         #200 Reset = 1;
+        Load = 1; // Load initial values again
+        Enable = 0; // Stop the timer
 
         #50 Reset = 0;
+        Load = 0; // Stop loading initial values
+        Enable = 1; // Start the timer again
 
-        #5990 $finish;
+        #6100 $finish;
     end
 
 endmodule
