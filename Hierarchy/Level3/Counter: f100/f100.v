b4fdf268a7943bfded0b3d65b613abb32b1d2d37
pedroagaeme <137459215+pedroagaeme@users.noreply.github.com>
2024-03-19 20:39:53 -0300

    Update f100.v

--- Hierarchy/Level3/Counter: f100/f100.v
+++ Hierarchy/Level3/Counter: f100/f100.v
@@ -3,13 +3,14 @@ module ContadorFreq100 (
     output reg Hz1        
 );
 
-reg [6:0] count = 7'b0000000; 
+  reg [25:0] count = 26'b00000000000000000000000000; 
 
 always @(posedge clk_100Hz) begin
-    if (count == 7'b1100100) begin 
-        count <= 7'b0; 
-        Hz1 <= ~Hz1; //inverte o sinal a cada ciclo de clock para obter 1 Hz
+  if (count == 26'b10111110101111000010000000) begin 
+        count <= 26'b0; 
+        Hz1 <= 1;
     end else begin
+        Hz1 <= 0;
         count <= count + 1; 
     end
 end
