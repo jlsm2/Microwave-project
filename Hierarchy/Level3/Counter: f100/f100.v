601158f046b6d77865c0159a30fe1fdca2b89d25
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-19 07:05:58 -0300

    Update f100.v

--- Hierarchy/Level3/Counter: f100/f100.v
+++ Hierarchy/Level3/Counter: f100/f100.v
@@ -1,16 +1,16 @@
 module ContadorFreq100 (
-    input wire clk_100Hz, // Clock de 100 Hz
-    output reg Hz1        // Saída de 1 Hz
+    input wire clk_100Hz,
+    output reg Hz1        
 );
 
-reg [6:0] count = 7'b0000000; // Contador de 7 bits em binário
+reg [6:0] count = 7'b0000000; 
 
 always @(posedge clk_100Hz) begin
-    if (count == 7'b1100100) begin // 100 em binário
-        count <= 7'b0; // Reinicia o contador quando atinge 100
-        Hz1 <= ~Hz1; // Inverte o sinal a cada ciclo de clock para obter 1 Hz
+    if (count == 7'b1100100) begin 
+        count <= 7'b0; 
+        Hz1 <= ~Hz1; //inverte o sinal a cada ciclo de clock para obter 1 Hz
     end else begin
-        count <= count + 1; // Incrementa o contador
+        count <= count + 1; 
     end
 end
 
