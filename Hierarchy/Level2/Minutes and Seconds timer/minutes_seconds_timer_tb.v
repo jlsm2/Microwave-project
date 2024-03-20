262e3a88bb339dc4ea0c60d476b1c6164c6cf100
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-20 09:59:45 -0300

    Update minutes_seconds_timer_tb.v

--- Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
+++ Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
@@ -5,10 +5,10 @@ module MinutesSecondsTimer_tb;
     reg CLK;
     reg Load;
     reg Enable;
-    reg Clearn; // New input for clear signal
-    reg [3:0] initial_seconds_units; // New input for initial seconds units value
-    reg [2:0] initial_seconds_tens; // New input for initial seconds tens value
-    reg [3:0] initial_minutes_units; // New input for initial minutes units value
+    reg Clearn; 
+    reg [3:0] initial_seconds_units; 
+    reg [2:0] initial_seconds_tens; 
+    reg [3:0] initial_minutes_units;
 
     wire [3:0] seconds_units;
     wire [2:0] seconds_tens;
@@ -19,10 +19,10 @@ module MinutesSecondsTimer_tb;
         .CLK(CLK),
         .Load(Load),
         .Enable(Enable),
-        .Clearn(Clearn), // Connect clear signal
-        .initial_seconds_units(initial_seconds_units), // Connect initial seconds units input
-        .initial_seconds_tens(initial_seconds_tens), // Connect initial seconds tens input
-        .initial_minutes_units(initial_minutes_units), // Connect initial minutes units input
+        .Clearn(Clearn), 
+        .initial_seconds_units(initial_seconds_units), 
+        .initial_seconds_tens(initial_seconds_tens),
+        .initial_minutes_units(initial_minutes_units),
         .seconds_units(seconds_units),
         .seconds_tens(seconds_tens),
         .minutes_units(minutes_units),
@@ -43,7 +43,7 @@ module MinutesSecondsTimer_tb;
     initial begin
         Load = 1;
         Enable = 0;
-        Clearn = 1; // Initially, Clearn is high
+        Clearn = 1;
 
         // Load initial values
         initial_seconds_units = 4'b0111;
@@ -63,9 +63,9 @@ module MinutesSecondsTimer_tb;
         Enable = 1;
 
         #5000
-        Clearn = 0; // Clear the timer
+        Clearn = 0;
         #10
-        Clearn = 1; // Set Clearn back to high
+        Clearn = 1;
 
         #6100 $finish;
     end
