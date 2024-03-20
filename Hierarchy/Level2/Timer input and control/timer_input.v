c9c0dd209565d69567bedc7a47991bab7469029b
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-20 10:02:25 -0300

    Update timer_input.v

--- Hierarchy/Level2/Timer input and control/timer_input.v
+++ Hierarchy/Level2/Timer input and control/timer_input.v
@@ -1,6 +1,6 @@
 module timer_input (
     input wire clk,
-    input wire enablen, // Changed from rst to enablen
+    input wire enablen, 
     input wire [9:0] switches,
     output reg [3:0] units_of_seconds,
     output reg [3:0] units_of_minutes,
@@ -9,14 +9,11 @@ module timer_input (
     output wire pgt_1Hz
 );
 
-// Internal signals
 wire [3:0] encoded_input;
 reg [3:0] last_input;
 wire [2:0] counter;
 wire mux_a;
 wire mux_b;
-
-// Instantiate modules
 
 priority_encoder_4x2 encoder (
     .enable(enablen),
@@ -27,7 +24,7 @@ priority_encoder_4x2 encoder (
 
 counter_0_to_7_non_recycling counter_inst (
     .clk(clk),
-    .rst(enablen), // Changed from rst to enablen
+    .rst(enablen), 
   .count(counter),
   .out(mux_a)
 );
@@ -43,24 +40,22 @@ ContadorFreq100 cf(
    .sel(enablen), 
    .Q(pgt_1Hz));
 
-always @(posedge clk or posedge ~enablen) begin // Changed from rst to enablen
-    if (~enablen) begin // Changed from rst to enablen
+always @(posedge clk or posedge ~enablen) begin 
+    if (~enablen) begin
         last_input <= encoded_input;
         units_of_seconds <= 4'b0000;
         units_of_minutes <= 4'b0000;
         tens_of_seconds <= 4'b0000;
-        loadn <= 1'b0; // Added loadn output
+        loadn <= 1'b0;
     end else begin
-        // Detect change in input
         if (encoded_input != last_input ) begin
-            // Update timer values
             if(encoded_input != 4'b1111) begin
                 tens_of_seconds <= units_of_seconds;
                 units_of_minutes <= tens_of_seconds;
                 units_of_seconds <= encoded_input;
             end
             last_input <= encoded_input;
-            loadn <= 1'b1; // Added loadn output
+            loadn <= 1'b1;
         end
     end
 end
