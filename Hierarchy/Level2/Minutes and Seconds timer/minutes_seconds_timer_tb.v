fe81dc6b771a3dd72b26949b64f24c4ce2fe2b94
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-03-20 08:40:02 -0300

    Update minutes_seconds_timer_tb.v

--- Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
+++ Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer_tb.v
@@ -1,27 +1,32 @@
-`include "MinutesSecondsTimer.v"
 `timescale 1s/1s
 
 module MinutesSecondsTimer_tb;
 
     reg CLK;
-    reg Reset;
-    reg Load; // New Load input
-    reg Enable; // New Enable input
+    reg Load;
+    reg Enable;
+    reg Clearn; // New input for clear signal
+    reg [3:0] initial_seconds_units; // New input for initial seconds units value
+    reg [2:0] initial_seconds_tens; // New input for initial seconds tens value
+    reg [3:0] initial_minutes_units; // New input for initial minutes units value
 
     wire [3:0] seconds_units;
     wire [2:0] seconds_tens;
     wire [3:0] minutes_units;
-    wire timer_done; // New timer_done output
+    wire timer_done;
 
     MinutesSecondsTimer uut (
         .CLK(CLK),
-        .Reset(Reset),
-        .Load(Load), // Connect Load input
-        .Enable(Enable), // Connect Enable input
+        .Load(Load),
+        .Enable(Enable),
+        .Clearn(Clearn), // Connect clear signal
+        .initial_seconds_units(initial_seconds_units), // Connect initial seconds units input
+        .initial_seconds_tens(initial_seconds_tens), // Connect initial seconds tens input
+        .initial_minutes_units(initial_minutes_units), // Connect initial minutes units input
         .seconds_units(seconds_units),
         .seconds_tens(seconds_tens),
         .minutes_units(minutes_units),
-        .timer_done(timer_done) // Connect timer_done output
+        .timer_done(timer_done)
     );
 
     initial begin
@@ -36,21 +41,31 @@ module MinutesSecondsTimer_tb;
     end
 
     initial begin
-        Reset = 1;
-        Load = 1; // Load initial values
-        Enable = 0; // Don't start the timer yet
+        Load = 1;
+        Enable = 0;
+        Clearn = 1; // Initially, Clearn is high
 
-        #10 Reset = 0;
-        Load = 0; // Stop loading initial values
-        Enable = 1; // Start the timer
+        // Load initial values
+        initial_seconds_units = 4'b0111;
+        initial_seconds_tens = 3'b101; 
+        initial_minutes_units = 4'b1000; 
 
-        #200 Reset = 1;
-        Load = 1; // Load initial values again
-        Enable = 0; // Stop the timer
+        #10
+        Load = 0;
+        Enable = 1;
 
-        #50 Reset = 0;
-        Load = 0; // Stop loading initial values
-        Enable = 1; // Start the timer again
+        #200
+        Load = 1;
+        Enable = 0;
+
+        #50
+        Load = 0;
+        Enable = 1;
+
+        #5000
+        Clearn = 0; // Clear the timer
+        #10
+        Clearn = 1; // Set Clearn back to high
 
         #6100 $finish;
     end
