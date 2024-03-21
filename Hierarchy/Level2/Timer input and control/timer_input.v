a2901b64f0c81c658d14cf77a355424e5f16d203
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-03-21 17:59:39 -0300

    Update timer_input.v

--- Hierarchy/Level2/Timer input and control/timer_input.v
+++ Hierarchy/Level2/Timer input and control/timer_input.v
@@ -1,6 +1,6 @@
 module timer_input (
     input wire clk,
-    input wire enablen, 
+    input wire enablen, // Changed from rst to enablen
     input wire [9:0] switches,
     output reg [3:0] units_of_seconds,
     output reg [3:0] units_of_minutes,
@@ -9,11 +9,14 @@ module timer_input (
     output wire pgt_1Hz
 );
 
+// Internal signals
 wire [3:0] encoded_input;
 reg [3:0] last_input;
 wire [2:0] counter;
 wire mux_a;
 wire mux_b;
+
+// Instantiate modules
 
 priority_encoder_4x2 encoder (
     .enable(enablen),
@@ -24,38 +27,41 @@ priority_encoder_4x2 encoder (
 
 counter_0_to_7_non_recycling counter_inst (
     .clk(clk),
-    .rst(enablen), 
-  .count(counter),
-  .out(mux_a)
+    .rst(~enablen), // Changed from rst to enablen
+	 .count(counter)
 );
  
-ContadorFreq100 cf( 
-  .clk_100Hz(clk),
-  .Hz1(mux_b) 
-  );
+f100 cf( 
+   .clk_100Hz(clk),
+   .Hz1(mux_b) 
+);
 
  mux2_to_1 mux(
    .a(mux_a), 
    .b(mux_b), 
    .sel(enablen), 
-   .Q(pgt_1Hz));
+   .Q(pgt_1Hz)
+);
 
-always @(posedge clk or posedge ~enablen) begin 
-    if (~enablen) begin
+always @(posedge clk) begin // Changed from rst to enablen
+
+	 if (~enablen) begin // Changed from rst to enablen
         last_input <= encoded_input;
-        units_of_seconds <= 4'b0000;
-        units_of_minutes <= 4'b0000;
-        tens_of_seconds <= 4'b0000;
-        loadn <= 1'b0;
+        units_of_seconds <= 4'b1111;
+        units_of_minutes <= 4'b1111;
+        tens_of_seconds <= 4'b1111;
+        loadn <= 1'b0; // Added loadn output
     end else begin
-        if (encoded_input != last_input ) begin
+        // Detect change in input
+        if (encoded_input != last_input) begin
+            // Update timer values
             if(encoded_input != 4'b1111) begin
                 tens_of_seconds <= units_of_seconds;
                 units_of_minutes <= tens_of_seconds;
                 units_of_seconds <= encoded_input;
             end
             last_input <= encoded_input;
-            loadn <= 1'b1;
+            loadn <= 1'b1; // Added loadn output
         end
     end
 end
