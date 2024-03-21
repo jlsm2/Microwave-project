9fde65f8b4c356f50b76e3b49b8fac04d05a1b9d
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-03-21 18:04:35 -0300

    Update minutes_seconds_timer.v

--- Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer.v
+++ Hierarchy/Level2/Minutes and Seconds timer/minutes_seconds_timer.v
@@ -1,27 +1,20 @@
-`include "mod10_counter.v"
-`include "mod6_counter.v"
-
-module MinutesSecondsTimer (
+module minutes_seconds_timer (
     input wire CLK,
     input wire Load,
     input wire Enable,
-    input wire Clearn, 
-    input wire [3:0] initial_seconds_units, 
-    input wire [2:0] initial_seconds_tens, 
-    input wire [3:0] initial_minutes_units, 
+    input wire Clearn,
+    input wire [3:0] initial_seconds_units,
+    input wire [3:0] initial_seconds_tens,
+    input wire [3:0] initial_minutes_units,
     output reg [3:0] seconds_units,
-    output reg [2:0] seconds_tens,
+    output reg [3:0] seconds_tens,
     output reg [3:0] minutes_units,
     output reg timer_done
 );
 
 reg [3:0] min_counter;
-reg [3:0] seconds_units_load; 
-reg [2:0] seconds_tens_load; 
-reg [3:0] minutes_units_load; 
-
 wire [3:0] sec_units_counter;
-wire [2:0] min_counter_output;
+wire [3:0] min_counter_output;
 
 mod10_counter mod10 (
     .CLK(CLK),
@@ -34,35 +27,47 @@ mod6_counter mod6 (
 );
 
 always @ (posedge CLK) begin
-    if (!Clearn) begin
-        seconds_units <= 4'b0000;
-        seconds_tens <= 3'b000;
-        minutes_units <= 4'b0000;
-        timer_done <= 1'b0;
-    end else if (Load) begin
-        seconds_units <= initial_seconds_units; 
-        seconds_tens <= initial_seconds_tens; 
-        minutes_units <= initial_minutes_units; 
-        seconds_units_load <= initial_seconds_units; 
-        seconds_tens_load <= initial_seconds_tens; 
-        minutes_units_load <= initial_minutes_units;
-    end else if (Enable && !(minutes_units == 4'b0000 && seconds_tens == 3'b000 && seconds_units == 4'b0000)) begin
-        if (seconds_tens == 3'b000 && seconds_units == 4'b0000) begin
-            minutes_units <= minutes_units - 1;
-            seconds_tens <= 3'b101;
-            seconds_units <= 4'b1001;
-        end else if (seconds_units == 4'b0000) begin
-            seconds_tens <= seconds_tens - 1;
-            seconds_units <= 4'b1001;
-        end else begin
-            seconds_units <= seconds_units - 1;
-        end
-    end
-    if (minutes_units == 4'b0000 && seconds_tens == 3'b000 && seconds_units == 4'b0000) begin
+	 if ((minutes_units == 4'b0000) && (seconds_tens == 4'b0000) && (seconds_units == 4'b0000)) begin
         timer_done <= 1'b1;
     end else begin
         timer_done <= 1'b0;
     end
+		  
+    if (!Clearn) begin
+        seconds_units <= 4'b1111;
+        seconds_tens <= 4'b1111;
+        minutes_units <= 4'b1111;
+        //assign initial_seconds_units = 4'b1111;
+        //assign initial_seconds_tens = 4'b1111;
+        //assign initial_minutes_units = 4'b1111;
+    end else if (Load) begin
+        seconds_units <= initial_seconds_units;
+        seconds_tens <= initial_seconds_tens;
+        minutes_units <= initial_minutes_units;
+    end else if (Enable && !(minutes_units == 4'b0000 && seconds_tens == 4'b0000 && seconds_units == 4'b0000)) begin
+				if (minutes_units > 4'b1001) begin
+					minutes_units <= 4'b0000;
+			  end
+			  if (seconds_units > 4'b1001) begin
+					seconds_units <= 4'b0000;
+			  end
+			  if (seconds_tens > 4'b1001) begin
+					seconds_tens <= 4'b0000;
+			  end
+        if (seconds_tens == 4'b0000 && seconds_units == 4'b0000 && minutes_units < 4'b1001) begin
+            minutes_units <= minutes_units - 1;
+            seconds_tens <= 4'b0101;
+            seconds_units <= 4'b1001;
+        end else if (seconds_units == 4'b0000 && seconds_tens < 4'b1001) begin
+            seconds_tens <= seconds_tens - 1;
+            seconds_units <= 4'b1001;
+        end else begin
+				if (seconds_units < 4'b1001) begin
+					seconds_units <= seconds_units - 1;
+				end
+        end
+    end
+	 
 end
 
 endmodule
