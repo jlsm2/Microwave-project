cd518063e34e18835b74b4ad888f2f59b666fcc6
João Marrocos <135621408+jlsm2@users.noreply.github.com>
2024-03-21 18:01:40 -0300

    Update counter_0_to_7_non_recycling.v

--- Hierarchy/Level3/Counter: 0–7 non-recycling/counter_0_to_7_non_recycling.v
+++ Hierarchy/Level3/Counter: 0–7 non-recycling/counter_0_to_7_non_recycling.v
@@ -1,8 +1,7 @@
 module counter_0_to_7_non_recycling(
     input clk,
     input rst,
-  output reg [2:0] count,
-  output reg out=0
+  output reg [2:0] count
 );
 
 always @(posedge clk or posedge rst) begin
@@ -11,10 +10,8 @@ always @(posedge clk or posedge rst) begin
     end else begin
         if (count == 3'b111) begin
             count <= 3'b000;
-        end else if (count == 3'b011) begin 
-          	out <=1;
-          end else begin
-            count <= count + 1;
+		  end else begin
+			   count <= count + 1;
         end
     end
 end
